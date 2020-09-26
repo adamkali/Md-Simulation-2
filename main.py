284354532e70346164b380b6dcf22eb8164b1dbc
Adam Kalinowski <me@Carbon.localdomain>
2020-09-25 20:47:59 -0400

    Added Equations of State

--- main.py
+++ main.py
@@ -1,88 +1,100 @@
-import time
 from sys import argv
 from random import uniform
 import matplotlib.pyplot as mp
 import numpy as np
 import particle_class as pc
-import matplotlib.animation as ap
-from mpl_toolkits.mplot3d import Axes3D
-
-"""====================================================================
-Initialization
-===================================================================="""
-
-#initialize the box
-box = float(argv[1])
 
 def initialize(box):
     xyz = open("input.xyz", "w")
 
-    for i in range(50):
+    for i in range(500):
         x, y, z = uniform(-box,box), uniform(-box,box), uniform(-box,box)
         print(x,y,z, file=xyz)
 
-initialize(box)
+def init_particles(coords):
 
-# Load in input.xy to initalize the
-coordinates = np.loadtxt("input.xyz")
+    # Finds the number of particles in the system
+    size = coords.shape
+    size = size[0]
 
-# Finds the number of particles in the system
-size = coordinates.shape
-size = size[0]
+    # Initialize a particle array that uses every single particle in the
+    # box as an index.
+    particle_array = []
+    for p in range(size):
+        coord = np.array(coords[p][:])
+        particle_array.append(pc.Particle(coord))
 
-# Initialize a particle array that uses every single particle in the
-# box as an index.
-particle_array = []
-for p in range(size):
-    coord = np.array(coordinates[p][:])
-    particle_array.append(pc.Particle(coord))
+    return particle_array
 
-fig = mp.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-adam = pc.Particle([0,0,0],[0,0,-5e9])
-sierra = pc.Particle([0,0,0],[0,0,5e9])
-
-particles = [adam,sierra]
-
-def init():
-    for p in particle_array:
-        ax.scatter(p.x_position,p.y_position,p.z_position, c='b', marker='o')
-
-h = (1-0)/100
-t = np.arange(0,1,h)
-total = len(t)
-
-time_array = []
-
-start_time = time.time()
-def animate(i):
-#for instance in t:
-    print(f"Iteration {i}/{total} starting...")
-    iteration_time = time.time()
-    mp.cla()
-    ax.set_ylim(-box,box)
-    ax.set_xlim(-box,box)
-    ax.set_zlim(-box,box)
-    temp = 0.0
+def init_eos(particles,U,box):
     kinetic_energy = 0.0
-    for p in particle_array:
+    for p in particles:
         kinetic_energy += p.get_kinetic()
     Temprature = (2/3)*kinetic_energy/1.38e-23
-    for p in particle_array:
-        for q in particle_array:
-            p.monte_carlo(q,Temprature,box)
-        ax.scatter(p.x_position,p.y_position,p.z_position, c='b', marker='o')
 
-    iteration_time_final = time.time()
-    time_array.append(iteration_time_final - iteration_time)
-    print(f"Iteration {i}/{total} Complete\n")
-    print(f"Time elapsed:\t\t{time.time()-start_time}s")
+    potential = 0.0
+    for p in particles:
+        for q in particles:
+            potential += p.leonard_jones(q)
+    U.append(potential)
 
-ani = ap.FuncAnimation(fig,animate,t,init_func=init)
-ani.save('mcAnimation.mp4', writer='ffmpeg', fps=60)
+    N = len(particles)
+    kb,a,b = 1.3806e-23,553.6,0.0305
+    Temprature_prime = Temprature * 358.38
+    Avo = 6.022e23
+    pv_ideal = N*kb*Temprature_prime
+    n = N/Avo
+    V = (2*box*1e-10)**3
+    pv_ideal = pv_ideal/(V - n*b)
+    return pv_ideal - (a*(n/V)**2), Temprature
 
-fig = mp.figure()
-my_plot = fig.add_subplot()
-mp.plot(time_array)
-mp.show()
+
+def eos(particles,Temp,box):
+
+    V = (2*box*1e-10)**3
+    n = len(particles)/6.022e23
+    R, a, b =  8.3145, 553.6, 0.0305
+    Temprature_prime = Temp * 358.38
+    pv_ideal = n*R*Temprature_prime
+    pv_ideal = pv_ideal/(V - n*b)
+
+    return pv_ideal - (a*(n/V)**2)
+
+def main():
+
+    box = float(argv[1])
+
+    initialize(box)
+
+    h = (1-0)/100
+    t = np.arange(0,1,h)
+
+    U = []
+    pressure = []
+
+    # Load in input.xy to initalize the system
+    coordinates = np.loadtxt("input.xyz")
+    particle_array = init_particles(coordinates)
+    P, Temp = init_eos(particle_array,U,box)
+    pressure.append(P)
+
+    for instance in range(len(t)):
+        potential = 0.0
+        for p in particle_array:
+            for q in particle_array:
+                p.monte_carlo(q,Temp,box)
+                potential += p.leonard_jones(q)
+        U.append(potential)
+        pressure.append(eos(particle_array,Temp,box))
+
+
+
+
+    print(f"Average Temprature\t\t\t{np.mean(Temp)}\nAverage LJ Energy\t\t\t{np.mean(U)}")
+
+    mp.plot(pressure)
+    mp.plot(U)
+    mp.show()
+
+if __name__ == "__main__":
+    main()
--- particle_class.py
+++ particle_class.py
@@ -5,20 +5,15 @@ import numpy as np
 
 class Particle():
 
-
-        #Define mass to determine Average Kinetic Energy and Temprature
-    mass = 1.9915e-20 # kg
-    c = 3e8
-
     #initiate the class
     def __init__(self, arr, vel_arr=None):
         self.x_position = arr[0]
         self.y_position = arr[1]
         self.z_position = arr[2]
         if vel_arr == None:
-            self.x_velocity = rand.uniform(-10,10)*1e8
-            self.y_velocity = rand.uniform(-10,10)*1e8
-            self.z_velocity = rand.uniform(-10,10)*1e8
+            self.x_velocity = rand.uniform(-4.04,4.04)
+            self.y_velocity = rand.uniform(-4.04,4.04)
+            self.z_velocity = rand.uniform(-4.04,4.04)
         else:
             self.x_velocity = vel_arr[0]
             self.y_velocity = vel_arr[1]
@@ -48,7 +43,7 @@ class Particle():
     def get_vels(self):
         return self.x_velocity,self.y_velocity,self.z_velocity
 
-    def leonard_jones(self, other, h1=0, h2=0, h3=0):
+    def leonard_jones(self, other):
 
         # Account for when there is a particle interating with itself
         if self == other:
@@ -56,156 +51,30 @@ class Particle():
 
         # Now for the meat of the model
         else:
-            X = (self.x_position - other.x_position) + h1
-            Y = (self.y_position - other.y_position) + h2
-            Z = (self.z_position - other.z_position) + h3
+            X = (self.x_position - other.x_position)
+            Y = (self.y_position - other.y_position)
+            Z = (self.z_position - other.z_position)
             R = sqrt(X**2 + Y**2 + Z**2)
             return 4*self.epsilon*((self.sigma/R)**12 - (self.sigma/R)**6)
 
-
-    def force(self,other):
-
-
-        hx, hy, hz = ((self.x_position-other.x_position + .001)/100),\
-                ((self.y_position-other.y_position + 0.001)/100 ), \
-                ((self.z_position-other.z_position + .001)/100)
-
-        F_x = (self.leonard_jones(other,h1=hx)-self.leonard_jones(other,h1=-hx))/(2*hx)
-        F_y = (self.leonard_jones(other,h2=hy)-self.leonard_jones(other,h2=-hy))/(2*hy)
-        F_z = (self.leonard_jones(other,h3=hy)-self.leonard_jones(other,h3=-hz))/(2*hz)
-        return np.array([F_x,F_y,F_z], float)
-
-    def verlet(self, other,step_size):
-
-
-        if self.get_poss() == other.get_poss():
-
-            print("-"*40 + "collision" + "-"*40)
-            self.x_position += -self.x_velocity*step_size
-            self.y_position += -self.y_velocity*step_size
-            self.z_position += -self.z_velocity*step_size
-            other.x_position += -other.x_velocity*step_size
-            other.y_position += -other.y_velocity*step_size
-            other.z_position += -other.z_velocity*step_size
-
-        verlet_force = self.force(other)
-        half_step = 0.5*step_size*verlet_force
-        vx_half = self.x_velocity + half_step[0]
-        vy_half = self.y_velocity + half_step[1]
-        vz_half = self.z_velocity + half_step[2]
-        self.x_position += step_size*vx_half
-        self.y_position += step_size*vy_half
-        self.z_position += step_size*vz_half
-        ghost = Particle(np.array([self.x_position,self.y_position,self.z_position], float),
-                [vx_half, vx_half, vz_half])
-        k = ghost.force(other)
-        self.x_velocity += step_size*k[0]
-        self.y_velocity += step_size*k[1]
-        self.z_velocity += step_size*k[2]
-        if self.x_velocity >= 50:
-            self.x_velocity = 50
-        if self.y_velocity >= 50:
-            self.y_velocity = 50
-        if self.z_velocity >= 50:
-            self.z_velocity = 50
-
     def get_kinetic(self):
-        mass = 1.9915e-20 # kg
-        return 0.5*mass*(self.x_velocity**2+self.y_velocity**2+self.z_velocity**2)*1e-20
+        mass = 30.113e-27 # kg
+        return 0.5*mass*(self.x_velocity**2+self.y_velocity**2+self.z_velocity**2)
 
     def monte_carlo(self, other, Temp, box):
         random_number= rand.uniform(0,1)
         kb = 1.3806e-23
-        check = True
-        while check:
-            random_array = [rand.uniform(-box,box),
-                    rand.uniform(-box,box),
-                    rand.uniform(-box,box)]
-            ghost = Particle(random_array,
-                    [self.x_velocity,self.y_velocity,self.z_velocity])
-            delta_pot = ghost.leonard_jones(other) - self.leonard_jones(other)
-            if delta_pot < 0:
-                check = False
-                self.x_position = ghost.x_position
-                self.y_position = ghost.y_position
-                self.z_position = ghost.z_position
-            elif np.exp(-delta_pot/(kb*Temp)) < random_number:
-                check = False
-                self.x_position = ghost.x_position
-                self.y_position = ghost.y_position
-                self.z_position = ghost.z_position
-"""
-    def boundaries(self,box):
-        if self.x_position >= box:
-            if self.y_position >= box:
-                if self.z_position >= box:
-                    return box,box,box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                elif self.z_position <= -box:
-                    return box,box,-box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                else:
-                    return box,box,self.z_position,-self.x_velocity,-self.y_velocity,self.z_velocity
-            elif self.y_position <= -box:
-                if self.z_position >= box:
-                    return box,-box,box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                elif self.z_position <= -box:
-                    return box,-box,-box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                else:
-                    return box,-box,self.z_position,-self.x_velocity,-self.y_velocity,self.z_velocity
-            else:
-                if self.z_position >= box:
-                    return box,self.y_position,box,-self.x_velocity,self.y_velocity,-self.z_velocity
-                elif self.z_position <= -box:
-                    return box,self.y_position,-box,-self.x_velocity,self.y_velocity,-self.z_velocity
-                else:
-                    return box,self.y_position,self.z_position,-self.x_velocity,self.y_velocity,self.z_velocity
-
-        elif self.x_position <= -box:
-            if self.y_position >= box:
-                if self.z_position >= box:
-                    return -box,box,box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                elif self.z_position <= -box:
-                    return -box,box,-box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                else:
-                    return -box,box,self.z_position,-self.x_velocity,-self.y_velocity,self.z_velocity
-            elif self.y_position <= -box:
-                if self.z_position >= box:
-                    return -box,-box,box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                elif self.z_position <= -box:
-                    return -box,-box,-box,-self.x_velocity,-self.y_velocity,-self.z_velocity
-                else:
-                    return -box,-box,self.z_position,-self.x_velocity,-self.y_velocity,self.z_velocity
-            else:
-                if self.z_position >= box:
-                    return -box,self.y_position,box,-self.x_velocity,self.y_velocity,-self.z_velocity
-                elif self.z_position <= -box:
-                    return -box,self.y_position,-box,-self.x_velocity,self.y_velocity,-self.z_velocity
-                else:
-                    return -box,self.y_position,self.z_position,-self.x_velocity,self.y_velocity,-self.z_velocity
-
-        elif self.y_position >= box:
-            if self.z_position >= box:
-                return self.x_position,box,box,self.x_velocity,-self.y_velocity,-self.z_velocity
-            elif self.z_position <= -box:
-                return self.x_position,box,-box,self.x_velocity,-self.y_velocity,-self.z_velocity
-            else:
-                return self.x_position,box,self.z_position,self.x_velocity,-self.y_velocity,self.z_velocity
-
-        elif self.y_position <= -box:
-            if self.z_position >= box:
-                return self.x_position,-box,box,self.x_velocity,-self.y_velocity,-self.z_velocity
-            elif self.z_position <= -box:
-                return self.x_position,-box,-box,self.x_velocity,-self.y_velocity,-self.z_velocity
-            else:
-                return self.x_position,-box,self.z_position,self.x_velocity,-self.y_velocity,self.z_velocity
-
-        elif self.z_position >= box:
-            return self.x_position,self.y_position,box,self.x_velocity,self.y_velocity,-self.z_velocity
-
-        elif self.z_position <= -box:
-            return self.x_position,self.y_position,-box,self.x_velocity,self.y_velocity,-self.z_velocity
-
-        else:
-            return self.x_position,self.y_position,self.z_position,self.x_velocity,self.y_velocity,self.z_velocity
-"""
-
-
+        random_array = [rand.uniform(-box,box),
+                rand.uniform(-box,box),
+                rand.uniform(-box,box)]
+        ghost = Particle(random_array,
+                [self.x_velocity,self.y_velocity,self.z_velocity])
+        delta_pot = ghost.leonard_jones(other) - self.leonard_jones(other)
+        if delta_pot <= 0:
+            self.x_position = ghost.x_position
+            self.y_position = ghost.y_position
+            self.z_position = ghost.z_position
+        elif np.exp(-delta_pot/(kb*Temp)) < random_number:
+            self.x_position = ghost.x_position
+            self.y_position = ghost.y_position
+            self.z_position = ghost.z_position
